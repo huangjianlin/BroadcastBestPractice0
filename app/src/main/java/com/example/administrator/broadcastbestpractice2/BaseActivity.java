6847d11c0abb8a98e5c31f47ef9120a87c427541
huangjianlin <474565628@qq.com>
2017-04-05 14:20:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/broadcastbestpractice2/BaseActivity.java
@@ -0,0 +1,63 @@
+package com.example.administrator.broadcastbestpractice2;
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by Administrator on 2017/4/5 0005.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+    private ForceOfflineReceiver receiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter=new IntentFilter();
+        intentFilter.addAction("com.example.broadcastbestpractice.FORCE_OFFLINE");
+        receiver=new ForceOfflineReceiver();
+        registerReceiver(receiver,intentFilter);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(receiver!=null){
+            unregisterReceiver(receiver);
+            receiver=null;
+        }
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+    class ForceOfflineReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            AlertDialog.Builder builder=new AlertDialog.Builder(context);
+            builder.setTitle("注意");
+            builder.setMessage("你已下线，请重新登录！");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCollector.finishAll();//销毁所有活动
+                    Intent intent=new Intent(context,LoginActivity.class);
+                    context.startActivity(intent);//重启LoginActivity
+                }
+            });
+            builder.show();
+
+        }
+    }
+
+}
